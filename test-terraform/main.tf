74e3d502f281fea95fa72e273014f1fa85e17421
BrandonErkamaa <brandonerkamaa@gmail.com>
2022-07-04 17:54:44 -0500

    Create structure

---
+++ test-terraform/main.tf
@@ -0,0 +1,38 @@
+#configure the Azure provider
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-github-rg"
+    storage_account_name = "erkamaatfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "text-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
---
+++ test-terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  type 		= string
+  default 	= "centralus"
+}
+variable "rg_name" {
+  type 		= string
+  default	= "test-rg"
+}
